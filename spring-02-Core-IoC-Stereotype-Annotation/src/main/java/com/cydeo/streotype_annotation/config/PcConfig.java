1f7b9c833ae447ebf734094e1ab07065e17951d4
Evgenia0707 <4030071@gmail.com>
2022-09-12 13:14:24 -0400

    Stereotype Annotation

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,10 @@
+package com.cydeo.streotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cydeo.streotype_annotation") // or ("com.cydeo") defin when to look for annotation
+public class PcConfig {
+
+}
---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/monitorfactory/AcerMonitor.java
@@ -0,0 +1,13 @@
+package com.cydeo.streotype_annotation.monitorfactory;
+
+public class AcerMonitor extends Monitor {
+    public AcerMonitor() {
+
+        super("25 inch beast","Acer",25);
+    }
+
+    public void drawPixelAt() {
+        System.out.println("Drawing pixel with Sony");
+
+    }
+}
