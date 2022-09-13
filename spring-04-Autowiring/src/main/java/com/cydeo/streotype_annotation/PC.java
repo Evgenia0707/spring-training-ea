6adaaa21be6268c1f0a22317f46961abb20e7b26
Evgenia0707 <4030071@gmail.com>
2022-09-13 11:52:09 -0400

    Autowiring

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -4,13 +4,23 @@ import com.cydeo.streotype_annotation.casefactory.Case;
 import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import com.cydeo.streotype_annotation.motherboardfactory.Motherboard;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
-public class PC {
+@Component // new PC(case monitor mother); injection whit obj not priminitive
+public class PC {// application -  create PC (obj) - in container now
+
+    //when create constructor (constr injection) -  Spring will do it- to provide obj- when we add something in feal - will inject directly
 
     private Case theCase;
+
     private Monitor monitor;
+
     private Motherboard motherboard;
+
+
+
 
     public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
         this.theCase = theCase;
--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Case.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Case.java
@@ -1,13 +1,23 @@
 package com.cydeo.streotype_annotation.casefactory;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@Component
 public abstract class Case {
 
     private String model;
     private String manufacturer;
     private String powerSupply;
+
+    @Autowired
+    private Dimensions dimensions; // not add to constr.
 
     public Case(String model, String manufacturer, String powerSupply) {
         this.model = model;
