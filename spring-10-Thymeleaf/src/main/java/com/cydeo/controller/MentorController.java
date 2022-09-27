2442554b7700c6e7c0ef76436efb5e0149c7e861
Evgenia0707 <4030071@gmail.com>
2022-09-26 20:17:14 -0400

    MVC Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -16,57 +16,27 @@ import java.util.List;
 @RequestMapping("/mentor")
 public class MentorController {
 
-//    @RequestMapping("/register") // localhost:8080/mentor/register  ( @RequestMapping("/mentor/register"))
-
-    @GetMapping("/register")       // localhost:8080/mentor/register -end point - wrote in browser - () execute model.add-> go to view have form - need to link to obj - <form th:object="${mentor}">
+    @GetMapping("/register")       // localhost:8080/mentor/register -end point
     public String register(Model model){
 
         List<String> batchList = Arrays.asList("JD1","JD2", "JD3");
-        model.addAttribute("batchList", batchList);      //batchList hold mentor obj
+        model.addAttribute("batchList", batchList);
 
-        //attribute mentor  will hold Obj what we filed in web brower
-        model.addAttribute("mentor", new Mentor());//create empty obj - give obj to view through attribute"mentor"
 
-//<form th:object="${mentor}"> - html - link to obj
-//<input type="text" th:field="*{firstName}"><br> -html -  will be assigned to mentor first name
-//when push register - new mentor will be saved in the value
+        model.addAttribute("mentor", new Mentor());
 
         return "mentor/mentor-register";//view
     }
 
-//     <form th:action="@{/mentor/confirm}"   th:object="${mentor}"> (from mentor-registr) create() and new html
 
 
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor, Model model ){
 
-    @PostMapping("/confirm")   //          mentor == attribute mentor
-    public String submitForm(@ModelAttribute("mentor") Mentor mentor, Model model ){//catching attr with @ModelAttribute and assign obj to mentor
 
-      //        return "mentor/mentor-register";//to see empty page when confirm + model.addAttribute
-
-        return "redirect:/mentor/register"; //go to method @GetMapping("/register") to end point
+        return "redirect:/mentor/register";
     }
 
 
 
-
-
-
-
-//     <form th:action="@{/mentor/confirm}" method="post"  th:object="${mentor}">
-    //need defein method
-//    @GetMapping("/confirm")
-//    public String submitForm2(){
-//
-//        return "mentor/mentor-confirmation";
-//    }
-
-
-    // in html - <option th:each="theBatch : ${batchList}" th:value="${theBatch}" th:text="theBatch"></option>
-    //                         loop -    what was chosen assign to theBatch           for print
-
-//    @RequestMapping("/drop") //localhost:8080/mentor/drop
-//    public String drop(){
-//
-//        return "mentor/register";
-//    }
 }
