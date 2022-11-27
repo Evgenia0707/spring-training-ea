cf99e1a4910ac7406772322265df8cd1792da9ba
Evgenia0707 <4030071@gmail.com>
2022-11-27 16:02:28 -0500

    Data CinemaLab

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/GenreRepository.java
@@ -0,0 +1,28 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Account;
+import com.cydeo.entity.Cinema;
+import com.cydeo.entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    // ------------------- JPQL QUERIES ------------------- //
+
+    //Write a JPQL query that return all genres
+    @Query("SELECT g FROM Genre g")
+    List<Genre> fetchAll();
+
+    // ------------------- Native QUERIES ------------------- //
+
+    //Write a native query that returns genres by containing name
+
+    @Query(value = "SELECT * FROM Genre WHERE name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+    List<Genre> retrieveGenresByContainingName(@Param("name") String name);
+
+}
